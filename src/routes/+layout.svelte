7152ed23c498501eb9784897e5888f36e6892e16
Rico <rico@cakeplabs.com>
2023-05-07 07:41:54 +0800

    update: add profile image, modify responsive

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <nav>
 	<div class="nav-container">
-		<a href="/" class="nav-logo" title="Back to Homepage">Gede Rico Wijaya</a>
+		<a href="/" class="nav-logo" title="Home">Gede Rico Wijaya</a>
 		<div class="nav-links">
 			<a href="https://gricowijaya.github.io" class="link" target="_blank">Blog</a>
 			<a href=" https://github.com/gricowijaya?tab=repositories" class="link" target="_blank"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,42 @@
-<!-- src/routes/index.svelte -->
+<script>
+	const Profile = new URL('images/profile.jpg', import.meta.url).href;
+</script>
+
 <svelte:head>
 	<title>Profile Web</title>
 </svelte:head>
 
 <div class="container">
 	<div class="info">
-		<div class="animate-background">
-			<div class="container info-container">
-				<div class="info-text">
-					<h1>
-						<p class="animate-text-1">IT Student Universitas Udayana</p>
-						<p class="text-2">Bangkit Cloud Computing Cohort</p>
-					</h1>
+		<div class="container info-container">
+			<h1>
+				<p class="animate-text-1">IT Student Universitas Udayana</p>
+				<p class="text-2">Bangkit Cloud Computing Cohort</p>
+			</h1>
+			<div class="profile-container">
+				<div class="image">
+					<img src={Profile} alt="profile" />
 				</div>
 				<div class="profile-biodata">
-					<div class="profile-container">
-						<h2>This is my Profile</h2>
-					</div>
+					<h2>
+						I'm Rico. I'm very passionate on learning IT and Security. It is fun thing in the world.
+						I'm currently learning Cloud Computing in Bangkit 2023 as Cloud Computing Cohort. I'm
+						also learning about Web Development and Security.
+					</h2>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 
-<div class="background-div" />
-
 <style>
 	.info {
 		text-align: center;
+		margin: auto;
 		position: relative;
 	}
 
 	.info-container {
-		height: 100%;
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
@@ -42,18 +46,18 @@
 
 	.container {
 		max-width: 1400px;
-		margin: 190px 0 0 2.5rem;
+		margin: 2.5rem;
 	}
-
-	info .profile-biodata {
-		position: relative;
-	}
-
-	.profile-biodata .profile-container {
+	.profile-container {
 		position: relative;
 		font: inherit;
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+		margin: 30px 0 0 2.5rem;
+	}
+
+	.profile-container .profile-biodata {
+		text-align: justify;
 	}
 
 	.info h1 {
@@ -64,7 +68,6 @@
 	.info h1 p {
 		position: relative;
 		font: inherit;
-		margin: 0;
 		text-align: left;
 		line-height: 130%;
 		width: fit-content;
@@ -75,28 +78,65 @@
 	.info h2 {
 		max-width: 65ch;
 		font-weight: 200;
-		margin-top: 0.5rem;
+	}
+	@media screen and (max-width: 1024px) {
+		.info h1 {
+			font-size: 3rem;
+		}
+		.profile-container {
+			flex-direction: row;
+			margin: 30px 0 0 0;
+		}
+		.profile-container .image {
+			margin-right: 20px;
+		}
+	}
+	@media screen and (max-width: 768px) {
+		.info h1 {
+			font-size: 2rem;
+		}
+		.info h1 p {
+			font-weight: 600;
+			font-size: 1.8rem;
+		}
+		.info h2 {
+			font-size: 1.6rem;
+		}
+		.profile-container {
+			flex-direction: column;
+			align-items: center;
+			margin-top: 20px;
+		}
+
+		.profile-container .image {
+			margin-right: 0;
+			margin-bottom: 20px;
+		}
 	}
 
-	.background-div {
-		background-color: var(--background);
-		width: 100%;
-		height: 56vh;
-		margin: -155px auto;
-	}
+	@media screen and (max-width: 400px) {
+		.profile-container .profile-biodata {
+			margin: 2rem;
+		}
+		.info h1 {
+			font-size: 2rem;
+		}
+		.info h1 p {
+			font-weight: 600;
+			font-size: 1.8rem;
+		}
+		.info h2 {
+			font-size: 1.6rem;
+		}
+		.profile-container {
+			flex-direction: column;
+			align-items: center;
+			margin-top: 20px;
+		}
 
-	.info .info-text {
-		position: relative;
-	}
-
-	.scroll-button {
-		display: flex;
-		border: none;
-		outline: none;
-		cursor: pointer;
-	}
-
-	.scroll-button .arrow {
-		margin-left: 0.5rem;
+		.profile-container .image {
+			margin-right: 0;
+			margin-bottom: 20px;
+		}
 	}
 </style>
