9c6e2392a8e0f79cebc28dd07dc8a7eccf3ca988
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-05-06 12:17:33 +0800

    profile web init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav>
+	<div class="nav-container">
+		<a href="/" class="nav-logo" title="Back to Homepage">Gede Rico Wijaya</a>
+		<div class="nav-links">
+			<a href="https://gricowijaya.github.io" class="link" target="_blank">Blog</a>
+			<a href=" https://github.com/gricowijaya?tab=repositories" class="link" target="_blank"
+				>Projects</a
+			>
+		</div>
+	</div>
+</nav>
+
+<div class="container">
+	<slot />
+</div>
+
+<style>
+	.container {
+		max-width: 1400px;
+		margin: 50px auto;
+	}
+</style>
---
+++ src/routes/contacts.js
@@ -0,0 +1,5 @@
+export const contacts = [{ 
+  slug: 'title',
+  title: 'title',
+  content: 'This is the title'
+}]
